4d497b2b6ebae266bab032d46e36420f2e476be1
Joshua Batista <jdbatista1999@gmail.com>
2019-10-06 21:39:05 -0400

    Euler5.cpp complete

---
+++ Euler5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+bool checksout(unsigned long int num){
+	for(unsigned int i = 1; i < 21; i++){
+		if(num%i != 0){
+			return false;
+		}
+	}
+	return true;
+}
+
+int main(){
+	for(unsigned long int i = 20; ; i+=20){
+		if(checksout(i)){
+			cout<<"Smallest number divis from 1-20: "<<i<<endl;
+			return 0;
+		}
+	}
+}
